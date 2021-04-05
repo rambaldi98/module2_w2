83fb4c6a2065642a62e21720b4339391a4beb362
rambaldi <diencong5998@gmail.com>
2021-04-05 08:59:29 +0700

    hien thi so nguyen to

--- src/HienThiSoNguyen2.java
+++ src/HienThiSoNguyen2.java
@@ -1,13 +1,15 @@
 
 
+
 import java.util.Scanner;
 
 public class HienThiSoNguyen2 {
 
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
         HienThiSoNguyen soNguyen = new HienThiSoNguyen();
         int n = 1;
+        System.out.println(soNguyen.a());
         while(n < 100){
             if(soNguyen.checkSNT(n)){
                 System.out.print(n +" ");
